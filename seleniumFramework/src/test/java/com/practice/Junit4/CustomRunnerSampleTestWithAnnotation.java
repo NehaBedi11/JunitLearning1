ccac1ad94b1add8f2c2d0ba57bdc796d86342589
Neha <neha@192.168.0.4>
2017-01-20 22:32:29 +0530

    Initial Commit

---
+++ seleniumFramework/src/test/java/com/practice/Junit4/CustomRunnerSampleTestWithAnnotation.java
@@ -0,0 +1,31 @@
+package com.practice.Junit4;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(CustomJunitRunner.class)
+public class CustomRunnerSampleTestWithAnnotation {
+	@Test
+    public void testListener(){
+
+    }
+
+    @Test
+    public void testFalseAssertion(){
+        assertTrue(false);
+    }
+
+    @Ignore
+    @Test
+    public void testIgnore(){
+
+    }
+
+    @Test
+    public void testException(){
+        throw new RuntimeException();
+    }
+}
